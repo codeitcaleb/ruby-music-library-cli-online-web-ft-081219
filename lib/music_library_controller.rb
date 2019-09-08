a29423397fde22369e68d955cc1cfb97ca87e817
Caleb Kavanaugh <calebck@hotmail.com>
2019-09-07 22:37:26 -0400

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -110,7 +110,7 @@ class MusicLibraryController
   
     if (1..Song.all.length).include?(input)
       song = list_of_songs[input + 1]
-      puts "Playing #{song.name} by #{song.artist.name}"
+      puts "Playing #{song.name} by #{song.artist.name}" if true
     end
      #binding.pry
   end
